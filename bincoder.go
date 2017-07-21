e00dccf19ad9ce9b6ffb18b4c2356a72d9d8661c
Erik Martino <erik.martino@gmail.com>
2017-07-21 12:45:00 +0200

    Initial commit

---
+++ bincoder.go
@@ -0,0 +1,101 @@
+package bincoder
+
+import "bufio"
+import "encoding/binary"
+
+// Bincoder that support basic builtin types
+type Bincoder interface {
+	ui16(f *uint16)
+	ui32(f *uint32)
+	i(f *int)
+	slice(
+		length func() int,
+		constructor func(int),
+		iterate func(int),
+	)
+	string(f *string)
+}
+
+// BinReader holds a bufio.Reader that is the source of unmarshalling
+type BinReader struct {
+	source *bufio.Reader
+}
+
+// BinWriter holds a bufio.Writer that is the target of marshalling
+type BinWriter struct {
+	target *bufio.Writer
+}
+
+func (coder BinReader) ui16(f *uint16) {
+	buf := [2]byte{}
+	coder.source.Read(buf[0:2])
+	*f = binary.LittleEndian.Uint16(buf[0:2])
+}
+
+func (coder BinWriter) ui16(f *uint16) {
+	buf := [2]byte{}
+	binary.LittleEndian.PutUint16(buf[:], *f)
+	coder.target.Write(buf[:])
+}
+
+func (coder BinReader) ui32(f *uint32) {
+	buf := [4]byte{}
+	coder.source.Read(buf[0:4])
+	*f = binary.LittleEndian.Uint32(buf[0:4])
+}
+
+func (coder BinWriter) ui32(f *uint32) {
+	buf := [4]byte{}
+	binary.LittleEndian.PutUint32(buf[:], *f)
+	coder.target.Write(buf[:])
+}
+
+func (coder BinReader) i(f *int) {
+	buf := [4]byte{}
+	coder.source.Read(buf[0:4])
+	*f = int(binary.LittleEndian.Uint32(buf[0:4]))
+}
+
+func (coder BinWriter) i(f *int) {
+	buf := [4]byte{}
+	binary.LittleEndian.PutUint32(buf[:], uint32(*f))
+	coder.target.Write(buf[:])
+}
+
+// reads length [4]byte, entries [length*sizeof(E)]byte
+func (coder BinReader) slice(
+	length func() int, constructor func(int), iterator func(int)) {
+	var size int
+	coder.i(&size)
+	constructor(size)
+	for i := 0; i < size; i++ {
+		iterator(i)
+	}
+}
+
+// writes length [4]byte, entries [length*sizeof(E)]byte
+func (coder BinWriter) slice(length func() int, constructor func(int),
+	iterator func(int)) {
+	size := length()
+	coder.i(&size) // writes the size of the slice
+	for i := 0; i < size; i++ {
+		iterator(i)
+	}
+}
+
+// reads size [4]byte, content [size]byte from source
+func (coder BinReader) string(f *string) {
+	var size int
+	coder.i(&size)
+	c := make([]byte, size)
+	coder.source.Read(c)
+	*f = string(c)
+}
+
+// writes size [4]byte, content [size]byte to target
+func (coder BinWriter) string(f *string) {
+	c := []byte(*f)
+	size := len(c)
+	coder.i(&size)
+	coder.target.Write(c)
+}
---
+++ bincoder_test.go
@@ -0,0 +1,144 @@
+package bincoder
+
+import (
+	"bufio"
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+// A is a type
+type foo struct {
+	a uint16
+	b uint32
+}
+
+type bar struct {
+	x    uint32
+	foo  foo
+	y    uint16
+	foos []foo
+	name string
+}
+
+type testCoder interface {
+	Bincoder
+	foo(f *foo)
+	bar(f *bar)
+	fooSlice(f *[]foo)
+}
+
+func (wire BinReader) foo(f *foo) {
+	f.encode(wire)
+}
+
+func (wire BinWriter) foo(f *foo) {
+	f.encode(wire)
+}
+
+func encodeFooSlice(f *[]foo, w testCoder) {
+	w.slice(
+		func() int { return len(*f) },
+		func(size int) { *f = make([]foo, size) },
+		func(i int) { w.foo(&(*f)[i]) },
+	)
+}
+
+func (wire BinReader) fooSlice(f *[]foo) {
+	encodeFooSlice(f, wire)
+}
+
+func (wire BinWriter) fooSlice(f *[]foo) {
+	encodeFooSlice(f, wire)
+}
+
+func (wire BinReader) bar(f *bar) {
+	f.encode(wire)
+}
+
+func (wire BinWriter) bar(f *bar) {
+	f.encode(wire)
+}
+
+func (f *foo) encode(w Bincoder) {
+	w.ui16(&f.a)
+	w.ui32(&f.b)
+}
+
+func (f *bar) encode(w testCoder) {
+	w.ui16(&f.y)
+	w.foo(&f.foo)
+	w.ui32(&f.x)
+	w.fooSlice(&f.foos)
+	w.string(&f.name)
+}
+
+func TestFoo_marshall(t *testing.T) {
+	o := foo{
+		a: 10,
+		b: 20,
+	}
+
+	var b bytes.Buffer
+	w := BinWriter{target: bufio.NewWriter(&b)}
+	w.foo(&o)
+	want := []byte{10, 0, 20, 0, 0, 0}
+	w.target.Flush()
+
+	got := b.Bytes()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("%q. got %v, want %v", "version", got, want)
+	}
+}
+
+func TestFoo_unmarshall(t *testing.T) {
+	var b bytes.Buffer
+	b.Write([]byte{87, 0, 42, 0, 0, 0})
+
+	w := BinReader{source: bufio.NewReader(&b)}
+	got := foo{}
+	w.foo(&got)
+
+	want := foo{
+		a: 87,
+		b: 42,
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("%q. got %v, want %v", "version", got, want)
+	}
+}
+
+func TestBar_marshall(t *testing.T) {
+	want := bar{
+		x: 42, y: 87,
+		foo: foo{
+			a: 10,
+			b: 20,
+		},
+		foos: []foo{
+			{
+				a: 11,
+				b: 12,
+			}, {
+				a: 13,
+				b: 14,
+			},
+		},
+		name: "Wire Marshall",
+	}
+	var b bytes.Buffer
+	writer := bufio.NewWriter(&b)
+	w := BinWriter{target: writer}
+	w.bar(&want)
+	writer.Flush()
+
+	reader := bufio.NewReader(&b)
+	r := BinReader{source: reader}
+	got := bar{}
+	r.bar(&got)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("%q. got %v, want %v", "version", got, want)
+	}
+}
